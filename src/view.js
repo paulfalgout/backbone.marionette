630b656736b7ad8e415c7c0142e9bf5a70c366bb
paulfalgout <paul@otterball.com>
2015-09-11 23:33:06 -0500

    Make region-manager a mixin
    Also make the view responsible for building the regions

--- src/view.js
+++ src/view.js
@@ -6,64 +6,44 @@
 // The standard view. Includes view events, automatic rendering
 // of Underscore templates, nested views, and more.
 Marionette.View = Backbone.View.extend({
-  regionClass: Marionette.Region,
-
-  destroyImmediate: false,
 
   // used as the prefix for child view events
   // that are forwarded through the layoutview
   childViewEventPrefix: 'childview',
 
-  // Setting up the inheritance chain which allows changes to
-  // Marionette.AbstractView.prototype.constructor which allows overriding
   constructor: function(options) {
-    options = options || {};
+    _.bind(this.render, this);
 
-    this._firstRender = true;
-    this._initializeRegions(options);
-
-    this.regions = this.regions || {};
-
-    _.bindAll(this, 'render');
-
-    // this exposes view options to the view initializer
-    // this is a backfill since backbone removed the assignment
-    // of this.options
-    // at some point however this may be removed
     this.options = _.extend({}, _.result(this, 'options'), options);
 
+    Marionette.MonitorDOMRefresh(this);
+
+    this._initRegions();
     var behaviors = Marionette._getValue(this.getOption('behaviors'), this);
     this._behaviors = Marionette.Behaviors(this, behaviors);
 
     Backbone.View.call(this, this.options);
 
     this.delegateEntityEvents();
-
-    Marionette.MonitorDOMRefresh(this);
   },
 
   // Serialize the view's model *or* collection, if
   // it exists, for the template
   serializeData: function() {
-    var data = {};
-
     if (!this.model && !this.collection) {
       return {};
     }
 
     // If we have a model, we serialize that
     if (this.model) {
-      data = this.serializeModel();
-
-    } else if (this.collection) {
-      // Otherwise, we serialize the collection,
-      // making it available under the `items` property
-      data = {
-        items: this.serializeCollection()
-      };
+      return this.serializeModel();
     }
 
-    return data;
+    // Otherwise, we serialize the collection,
+    // making it available under the `items` property
+    return {
+      items: this.serializeCollection()
+    };
   },
 
   // Serialize a collection by cloning each of
@@ -85,14 +65,10 @@ Marionette.View = Backbone.View.extend({
 
     this.triggerMethod('before:render', this);
 
-    if (this._firstRender) {
-      // if this is the first render, don't do anything to
-      // reset the regions
-      this._firstRender = false;
-    } else {
-      // If this is not the first render call, then we need to
-      // re-initialize the `el` for each region
-      this._reInitializeRegions();
+    // If this is not the first render call, then we need to
+    // re-initialize the `el` for each region
+    if (this.isRendered) {
+      this._reInitRegions();
     }
 
     this._renderTemplate();
@@ -142,102 +118,33 @@ Marionette.View = Backbone.View.extend({
     return this;
   },
 
-  // Add a single region, by name, to the layoutView
-  addRegion: function(name, definition) {
-    var regions = {};
-    regions[name] = definition;
-    return this.addRegions(regions)[name];
-  },
-
-  // Add multiple regions as a {name: definition, name2: def2} object literal or
-  // a function that evaluates to such literal
-  addRegions: function(regions) {
-    return this._addRegions(regions, arguments);
-  },
-
-  _addRegions: function(regions, parameters) {
-    // Enable regions to be a function
-    regions = Marionette._getValue(regions, this, parameters);
-
-    // Normalize region selectors hash to allow
-    // a user to use the @ui. syntax.
-    regions = this.normalizeUIValues(regions, ['selector', 'el']);
-
-    // Add the regions definitions to the regions property
-    this.regions = _.extend({}, this.regions, regions);
-
-    return this._buildRegions(regions);
-  },
-
-  // Remove a single region from the LayoutView, by name
-  removeRegion: function(name) {
-    delete this.regions[name];
-    return this.regionManager.removeRegion(name);
-  },
-
-  showChildView: function(regionName, view, options) {
-    var region = this.getRegion(regionName);
-    return region.show.apply(region, _.rest(arguments));
-  },
-
-  getChildView: function(regionName) {
-    return this.getRegion(regionName).currentView;
-  },
-
-  // Checks to see if layout contains region
-  // Accepts the region name
-  // hasRegion('main')
-  hasRegion: function(region) {
-    return !!this.getRegion(region);
-  },
-
-  // Provides alternative access to regions
-  // Accepts the region name
-  // getRegion('main')
-  getRegion: function(region) {
-    return this.regionManager.get(region);
-  },
-
-  // Get all regions
-  getRegions: function() {
-    return this.regionManager.getRegions();
-  },
-
-  // Enable easy overriding of the default `RegionManager`
-  // for customized region interactions and business specific
-  // view logic for better control over single regions.
-  getRegionManager: function() {
-    return new Marionette.RegionManager();
+  // https://github.com/jashkenas/backbone/blob/1.2.3/backbone.js#L1235
+  // Remove this view’s element from the document and all event
+  // listeners attached to it. Exposed for subclasses using an
+  // alternative DOM manipulation API.
+  _removeElement: function() {
+    this.$el.remove();
   },
 
   // Handle destroying regions, and then destroy the view itself.
   destroy: function() {
     if (this.isDestroyed) { return this; }
 
-    // #2134: remove parent element before destroying the child views, so
-    // removing the child views doesn't retrigger repaints
-    if (this.getOption('destroyImmediate') === true) {
-      this.$el.remove();
-    }
-    this.regionManager.destroy();
-
     var args = _.toArray(arguments);
 
     this.triggerMethod.apply(this, ['before:destroy'].concat(args));
 
-    // mark as destroyed before doing the actual destroy, to
-    // prevent infinite loops within "destroy" event handlers
-    // that are trying to destroy other views
+    this._removeElement();
+
     this.isDestroyed = true;
-    this.triggerMethod.apply(this, ['destroy'].concat(args));
-
-    // unbind UI elements
-    this.unbindUIElements();
-
     this.isRendered = false;
 
-    // remove the view from the DOM
-    this.remove();
+    this.unbindUIElements();
+    this.removeRegions();
+
+    this.triggerMethod.apply(this, ['destroy'].concat(args));
+
+    this.stopListening();
 
     // Call destroy on each behavior after
     // destroying the view.
@@ -246,64 +153,10 @@ Marionette.View = Backbone.View.extend({
     _.invoke(this._behaviors, 'destroy', args);
 
     return this;
-  },
-
-  // Internal method to initialize the regions that have been defined in a
-  // `regions` attribute on this layoutView.
-  _initializeRegions: function(options) {
-    this._initRegionManager();
-
-    this._addRegions(this.regions, [options]);
-    this._addRegions(this.getOption.call(options, 'regions'), [options]);
-  },
-
-  // internal method to build regions
-  _buildRegions: function(regions) {
-    var defaults = {
-      regionClass: this.getOption('regionClass'),
-      parentEl: _.partial(_.result, this, 'el')
-    };
-
-    return this.regionManager.addRegions(regions, defaults);
-  },
-
-  // Internal method to re-initialize all of the regions by updating
-  // the `el` that they point to
-  _reInitializeRegions: function() {
-    this.regionManager.invoke('reset');
-  },
-
-  // Internal method to initialize the region manager
-  // and all regions in it
-  _initRegionManager: function() {
-    this.regionManager = this.getRegionManager();
-    this.regionManager._parent = this;
-
-    this.listenTo(this.regionManager, 'before:add:region', function(name) {
-      this.triggerMethod('before:add:region', name);
-    });
-
-    this.listenTo(this.regionManager, 'add:region', function(name, region) {
-      this[name] = region;
-      this.triggerMethod('add:region', name, region);
-    });
-
-    this.listenTo(this.regionManager, 'before:remove:region', function(name) {
-      this.triggerMethod('before:remove:region', name);
-    });
-
-    this.listenTo(this.regionManager, 'remove:region', function(name, region) {
-      delete this[name];
-      this.triggerMethod('remove:region', name, region);
-    });
-  },
-
-  _getImmediateChildren: function() {
-    return _.chain(this.regionManager.getRegions())
-      .pluck('currentView')
-      .compact()
-      .value();
   }
+
 });
 
 _.extend(Marionette.View.prototype, Marionette.ViewMixin);
+
+_.extend(Marionette.View.prototype, Marionette.RegionsMixin);
